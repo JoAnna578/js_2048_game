10e47ef22fb1247c5ec0b2993ae36ac0e8519009
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-23 17:54:59 +0200

    feat: implement main.js to connect Game class with UI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,70 @@
-'use strict';
+// src/scripts/main.js
+import { Game } from '../modules/Game.class.js';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const startBtn = document.querySelector('.button.start');
+const restartBtn = document.querySelector('.button.restart');
+const scoreEl = document.querySelector('.game-score');
+const messageContainer = document.querySelector('.message-container');
+const loseMessage = document.querySelector('.message-lose');
+const winMessage = document.querySelector('.message-win');
+const startMessage = document.querySelector('.message-start');
 
-// Write your code here
+const game = new Game();
+
+// Funkcja do aktualizacji UI
+function render() {
+  const board = game.getState();
+  scoreEl.textContent = game.getScore();
+
+  // Aktualizacja statusu gry
+  if (game.getStatus() === 'win') {
+    winMessage.classList.remove('hidden');
+  } else {
+    winMessage.classList.add('hidden');
+  }
+
+  if (game.getStatus() === 'lose') {
+    loseMessage.classList.remove('hidden');
+  } else {
+    loseMessage.classList.add('hidden');
+  }
+
+  startMessage.classList.add('hidden');
+  // TODO: Aktualizacja komórek na planszy
+}
+
+// Obsługa przycisku Start
+startBtn.addEventListener('click', () => {
+  game.start();
+  render();
+  startBtn.classList.add('hidden');
+  restartBtn.classList.remove('hidden');
+});
+
+// Obsługa przycisku Restart
+restartBtn.addEventListener('click', () => {
+  game.restart();
+  render();
+});
+
+// Obsługa klawiszy strzałek
+document.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    default:
+      return;
+  }
+  render();
+});
+
